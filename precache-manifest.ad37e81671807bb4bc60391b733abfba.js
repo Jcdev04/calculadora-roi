ce1e37c5a7599c113a8d45d384af3c23586e539e
Jesús Castillo Vidal <jcdev2004@gmail.com>
2022-12-06 17:56:10 -0500

    Updates

---
+++ precache-manifest.ad37e81671807bb4bc60391b733abfba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b734111381cadd729428",
+    "url": "/calculadora-roi/static/css/main.22f9db85.chunk.css"
+  },
+  {
+    "revision": "b734111381cadd729428",
+    "url": "/calculadora-roi/static/js/main.d88feeca.chunk.js"
+  },
+  {
+    "revision": "b017708c06f301c7269b",
+    "url": "/calculadora-roi/static/js/runtime~main.70c240b4.js"
+  },
+  {
+    "revision": "cf96e10ae516a89ccb49",
+    "url": "/calculadora-roi/static/js/2.4f18f96e.chunk.js"
+  },
+  {
+    "revision": "4a89201314cb88630e58",
+    "url": "/calculadora-roi/static/js/3.a8e713bc.chunk.js"
+  },
+  {
+    "revision": "8b76192e392023324af41667782167ef",
+    "url": "/calculadora-roi/index.html"
+  }
+];
